61aa5d74834e80be418dfb35b62ecbd4a0c9c8f0
mohamedkhaledhafez <mohamedkhaled97858@gmail.com>
2021-07-29 18:11:21 +0200

    add gsap functionality

--- js/main.js
+++ js/main.js
@@ -35,3 +35,37 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+
+links.map((link) => {
+  if (!link) return;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const el = document.getElementById(id);
+
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  });
+});
+
+// Make gsap
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: 200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
